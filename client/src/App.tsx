a2d659d16f5cd706ea9079eed3000e7783db0624
Andrei Bakos <Andrei.Bakos@theaccessgroup.com>
2025-08-29 15:48:42 +0300

    Deploy fix for app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import IframeTool from "@/pages/iframe-tool";
 import NotFound from "@/pages/not-found";
+import { useHashLocation } from "wouter/use-hash-location";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={IframeTool}/>
-      <Route component={NotFound} />
-    </Switch>
+    <Router hook={useHashLocation}>
+      <Switch>
+        <Route path="/" component={IframeTool} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
@@ -20,7 +23,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRouter />
       </TooltipProvider>
     </QueryClientProvider>
   );
